20e753c99232b46599ebb008db1d472809762d83
mikesm11 <34034518+mikesm11@users.noreply.github.com>
2019-01-23 15:55:33 +0100

    After ISE session

--- ise-code/isepolicy.py
+++ ise-code/isepolicy.py
@@ -2,7 +2,7 @@
 """isepolicy - edit this file
 This is your starting point for the 0day workflow  Mission.
 Edit this file to
- - 
+ -
 There are a few places to edit (search for MISSION comments)
 
 Script Dependencies:
@@ -37,22 +37,25 @@ except:
 
 
 #TODO: Enter all authentication info
-ISE_ERSUSER=""
-ISE_ERSPASSWORD=""
-ISE_HOSTNAME=""
+ISE_ERSUSER="ersadmin"
+ISE_ERSPASSWORD="C1sco12345"
+ISE_HOSTNAME="198.18.133.27:9060"
 #TODO: Get the ISE URL setup
-url = "https://" + ISE_ERSUSER + ":" + ISE_ERSPASSWORD + "@" + ISE_HOSTNAME + "PUT_THE_RIGHT_API_ENDPOINT_HERE"
+url = "https://" + ISE_ERSUSER + ":" + ISE_ERSPASSWORD + "@" + ISE_HOSTNAME + "/ers/config/ancpolicy"
 
 #Let's create content headers
 headers = {
     'content-type': "application/json",
     'accept': "application/json"
     }
-
-#TODO:create the GET request 
-response = requests.request("ENTER THE HTTP METHOD", url, verify=False, headers=headers)
+namelist={}
+#TODO:create the GET request
+response = requests.request("GET", url, verify=False, headers=headers)
 if(response.status_code == 200):
      #TODO: Put the logic to parse the response to print the name of the ANC policies
+     resp_json=response.json()
+     for newlist in resp_json["SearchResult"]["resources"]:
+         namelist=newlist["name"]
+     print("Name of the ANC policy exits in system:",namelist)
 else:
         print("An error has ocurred with the following code %(error)s" % {'error': response.status_code})
-
